950d850041f560e00df9b04bcbec47ad099d1cc5
lizbur10 <liz.burton147@gmail.com>
2018-01-12 11:07:58 -0500

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,31 @@
+class Show < ActiveRecord::Base
+    def self.highest_rating
+        Show.maximum("rating")
+    end
+
+    def self.most_popular_show
+        max_rating = highest_rating
+        Show.find_by(rating: max_rating)
+    end
+
+    def self.lowest_rating
+        Show.minimum("rating")
+    end
+
+    def self.least_popular_show
+        min_rating = lowest_rating
+        Show.find_by(rating: min_rating)
+    end
+
+    def self.ratings_sum
+        Show.sum("rating")
+    end
+
+    def self.popular_shows
+        Show.where("rating > 5")
+    end
+
+    def self.shows_by_alphabetical_order
+        Show.order(:name)
+    end
+end
---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,8 @@
+class CreateShows < ActiveRecord::Migration
+    create_table :shows do | s |
+        s.string :name
+        s.string :network
+        s.string :day
+        s.integer :rating
+    end
+end
